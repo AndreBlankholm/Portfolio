b731fcfa1ea473f05faebadc3ab96f74e86036ea
AndreBlankholm <ablankholm@gmail.com>
2023-04-25 18:53:19 -0500

    modal styling

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,29 +1,39 @@
 import React from "react";
-import './Modal.css';
-
+import "./Modal.css";
 
 function Modal(props) {
-  const {currentProject, onClose} = props;
-  
+  const { currentProject, onClose } = props;
+
   return (
     // JSX
-    
-    
-    <div className="modalBackdrop" >
+
+    <div className="modalBackdrop">
       <div className="modalContainer">
         <h3 className="modalTitle">{currentProject.title}</h3>
-        <img src={require(`../../assests/images/img_folder/${currentProject.img}`)} alt={currentProject.alt} />
-        <p>Description:  {currentProject.description}</p>
+        <img
+          src={require(`../../assests/images/img_folder/${currentProject.img}`)}
+          alt={currentProject.alt}
+        />
         <div>
+          <p className="description">Description: {currentProject.description}</p>
+        </div>
+        <div className="hyperLinks">
           <p>
-          <a href={currentProject.github}>GitHub: {currentProject.github}</a>
+            Links
           </p>
           <p>
-          <a href={currentProject.deployment}>Deployment: {currentProject.deployment}</a>
+            <a href={currentProject.github}>GitHub: {currentProject.github}</a>
           </p>
-          
-          </div>        
-        <button type="button" onClick={() => onClose(currentProject.id)}>Close this modal</button>
+          <p>
+            <a href={currentProject.deployment}>
+              Deployment: {currentProject.deployment}
+            </a>
+          </p>
+        </div>
+
+        <button type="button" onClick={() => onClose(currentProject.id)}>
+          Close this modal
+        </button>
       </div>
     </div>
   );
