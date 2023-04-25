588c93283dedc048b586e142bcd840d3b48e049c
AndreBlankholm <ablankholm@gmail.com>
2023-04-24 20:54:35 -0500

    added images and material ui card to render projects

--- src/components/Portfolio/Portfoilo.js
+++ src/components/Portfolio/Portfoilo.js
@@ -1,25 +1,30 @@
-import React, { useState } from "react";
+import React from "react";
 import "./Portfolio.css";
+import Project from "../Project/Project";
+
 
 function Portfolio() {
 
-    
   const projects = [
     {
       title: "Everything Pizza",
       img_folder: "everything-pizza.png",
       description: "lorem itssum",
+      alt: "hello",
       tools: "JavaScript, Node & Express, MySQL, Bootstrap, Handlebars.js",
+
     },
     {
       title: "Weather Dashboard",
       img_folder: "weather-dash.png",
       description: "lorem itssum weather deashboard extra stuff always and forever",
+      alt: "",
       tools: "JavaScript, Node & Express, MySQL, Bootstrap, Handlebars.js",
     },
     {
       title: "Everything Pizza",
-      img_folder: "git-it-done",
+      img_folder: "git-it-done.png",
+      alt: "goodbye",
       description: "lorem itssum  everything pizza stuff gagas me with a spoon",
       tools: "MySQL, Bootstrap, Handlebars.js",
     },
@@ -28,6 +33,9 @@ function Portfolio() {
   return (
     <div className="portfolio">
       <h1>Portfolio</h1>
+    {projects.map((project, index) => (
+      <Project project={project}></Project>
+    ))}
     </div>
   );
 }
