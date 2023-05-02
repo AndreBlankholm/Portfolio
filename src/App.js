84171f773053b40fb0707841c01c092ea4c201a3
AndreBlankholm <ablankholm@gmail.com>
2023-05-02 15:37:04 -0500

    lost

--- src/App.js
+++ src/App.js
@@ -14,7 +14,8 @@ function App() {
         <NavBar />
         <div className="content-container">
           <Routes>
-            <Route path="/portfolio" element={<About />} />
+            <Route path="/" element={<About />} />
+            <Route path="/portfolio/portfolio" element={<About />} />
             <Route path="/about" element={<About />} />
             <Route path="/contact" element={<Contact />} />
             <Route path="/resume" element={<Resume />} />
